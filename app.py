15b05f7f7a3b381199e31f2a13ff4343b21e272c
toddhnstl <50678522+toddhnstl@users.noreply.github.com>
2019-11-25 20:12:41 -0600

    moving to functions

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import mission_to_mars
+import mars_functions
 
 app = Flask(__name__)
 
--- mission_to_mars.py
+++ mission_to_mars.py
@@ -9,126 +9,61 @@ import pandas as pd
 import requests
 import time
 import re
-
+import mars_functions
 
 from bs4 import BeautifulSoup as bs
 from splinter import Browser
 
 
-def init_browser():
-    # @NOTE: Replace the path with your actual path to the chromedriver
-    # executable_path = {"executable_path": "/usr/local/bin/chromedriver"}
-    executable_path = {'executable_path': 'chromedriver.exe'}
-    return Browser("chrome", **executable_path, headless=False)
 
-def scrape():
-    # # Scrape everything
-    #
+##
+#scrape_news 
+#  returns news_title, news_p
+def scrape_news():
+  print("IN call to SCRAPE NEWS")
+  # site 1 - "https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
+  news_url = "https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest" # probably need to replace this since it redirects
+  news_response = requests.get(news_url)
+  time.sleep(2)
 
-    # In[2]:
+  # use beautiful soup to parse the url above
+  news_soup = bs(news_response.text, 'html.parser')
+  #newPSoup = bs(news_response.text, 'html.parser')
 
-
-    # this dictionary will hold everything we pull from all the sites
-    scraped_data = {}
-
-
-    # In[3]:
-
-
-    # site 1 - "https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
-    news_url = "https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest" # probably need to replace this since it redirects
-    news_response = requests.get(news_url)
-    time.sleep(2)
-
-    # use beautiful soup to parse the url above
-    news_soup = bs(news_response.text, 'html.parser')
-    #newPSoup = bs(news_response.text, 'html.parser')
+  executable_path = {'executable_path': 'chromedriver.exe'}
+  browser = Browser('chrome', **executable_path, headless=False)
 
 
-    # In[4]:
-
-
-    # # example_title_div = '<div class="content_title"><a href="/news/8520/nasas-mars-2020-rover-tests-descent-stage-separation/" target="_self">NASAs Mars 2020 Rover Tests Descent-Stage Separation</a></div>'
-    # # example_paragraph_div = '<div class="article_teaser_body">A crane lifts the rocket-powered descent stage away from NASAs Mars 2020 rover after technicians tested the pyrotechnic charges that separate the two spacecraft.</div>'
-
-
-    # # use bs to find() the example_title_div and filter on the class_='content_tile'
-
-
-    # # presults = newPSoup.find_all('li', class_='slide')
-    # # print(presults)
-
-    # results = news_soup.find('div', class_='content_title')
-    # print("==== News Title =====")
-    # print(results)
-    # print("")
-    # news_title = results.a.text
-    # news_title = news_title.replace('\n', '')
-    # print(news_title)
-    # scraped_data['news_title'] = news_title #- load the dataframe with Key/value pair
-    # print("=========")
-    # print("")
-    # print("")
-
-    # # use bs to find() the example_title_div and filter on the class_='article_teaser_body'
-    # presults = news_soup.find('div', class_='article_teaser_body')
-    # print("====Paragraph=====")
-    # print(presults)
-    # print("=========")
-    # print("")
-    # print("")
-
-    # # # news_p = "FILL IN THE PARAGRAPH"
-    # # # scraped_data['news_p'] = news_p
-
-
-    # # I'm getting nowhere with just BS.  Try site 1 with splinter
-    #
-
-    # In[5]:
-
-
-    executable_path = {'executable_path': 'chromedriver.exe'}
-    browser = Browser('chrome', **executable_path, headless=False)
-
-
-    # In[ ]:
+  browser.visit(news_url)
+  time.sleep(3)
+  html = browser.html
+  soup = bs(html, 'html.parser')
 
 
 
+  # Get an article block.
+  art_teaser_body = soup.find('li', class_='slide')
+
+  # Parse out the title
+  title = art_teaser_body.find('div',  class_='content_title')
+  news_title = title.text
+
+  # Parse out the body text
+  news_p = art_teaser_body.find('div',  class_='article_teaser_body').text
+
+  
+  # scraped_data['news_title'] = news_title
+  # scraped_data['news_p'] = news_p
+  browser.quit()  
+  return(news_title, news_p)
 
 
-    # In[6]:
-
-
-    browser.visit(news_url)
-    time.sleep(3)
-    html = browser.html
-    soup = bs(html, 'html.parser')
-
-
-
-    # Get an article block.
-    art_teaser_body = soup.find('li', class_='slide')
-
-    # Parse out the title
-    title = art_teaser_body.find('div',  class_='content_title')
-    news_title = title.text
-
-    # Parse out the body text
-    news_p = art_teaser_body.find('div',  class_='article_teaser_body').text
-
-
-
-    # In[7]:
-
-
-    scraped_data['news_title'] = news_title
-    scraped_data['news_p'] = news_p
-
-
-    # In[8]:
-
+##
+# JPL
+#
+#
+def scrape_jpl():
+    print("in scrape JPL")
     # site 2 - https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars
     base_url = 'https://www.jpl.nasa.gov'
     # # use splinter to connect to the url and navigate, then use bs4 to repeat what you did in site 1
@@ -153,12 +88,12 @@ def scrape():
     print(featured_image_caption)
 
     # Load it to the dataframe
-    scraped_data['featured_image_url'] = featured_image_url
-    scraped_data['featured_image_caption'] = featured_image_caption
+    # scraped_data['featured_image_url'] = featured_image_url
+    # scraped_data['featured_image_caption'] = featured_image_caption
+    
+    return(featured_image_url, featured_image_caption)
 
-    # In[9]:
-
-
+def scrape_twitter():
     # site 3 - https://twitter.com/marswxreport?lang=en
 
     # grab the latest tweet and be careful its a weather tweet
@@ -166,10 +101,12 @@ def scrape():
     # Example:
     #mars_weather = 'Sol 1801 (Aug 30, 2017), Sunny, high -21C/-5F, low -80C/-112F, pressure at 8.82 hPa, daylight 06:09-17:55'
     # These seem to always start with 'InSight Sol'
+    print("in scrape_twitter")
     time.sleep(3)
 
     twit_url = 'https://twitter.com/marswxreport?lang=en'
-
+    executable_path = {'executable_path': 'chromedriver.exe'}
+    browser = Browser('chrome', **executable_path, headless=False)
     browser.visit(twit_url)
     html = browser.html
     soup = bs(html, 'html.parser')
@@ -207,10 +144,6 @@ def scrape():
             first = first + " hPa" # add back units of measure
             p_text = first
 
-
-            # Store it in the dataframe
-            scraped_data['weather_data'] = p_text
-    #         print("")
             break # Once we confirm we found a weather post with InSight, exit loop.
 
 
@@ -218,10 +151,13 @@ def scrape():
     #     print("")
 
     # print("End of tweet loop")
+    browser.quit()  
+    return(p_text)
 
-
-    # In[10]:
-
+##
+# Facts table
+#
+def  scrape_facts():
 
     # site 4 -
     facts_url = 'https://space-facts.com/mars/'
@@ -253,20 +189,19 @@ def scrape():
     # store it to the string...
     facts_html_str = facts_df.to_html(index=False, justify='center', escape=False, table_id='facts_table')
     # print(facts_html_str)
-
-    scraped_data['facts_html_str'] = facts_html_str
-
-
-    # In[11]:
+    return (facts_html_str)
 
 
-    # site 5
-
+##
+#S
+def scrape_img_urls():
     # set base URL for this site
     astro_base_url = 'https://astrogeology.usgs.gov'
 
     # use bs4 to scrape the title and url and add to dictionary
     hemi_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    executable_path = {'executable_path': 'chromedriver.exe'}
+    browser = Browser('chrome', **executable_path, headless=False)    
     browser.visit(hemi_url)
     time.sleep(5)
     html = browser.html
@@ -302,40 +237,41 @@ def scrape():
         ll_hemi_image_urls.append({"title": title, "img_url": img_url})
 
     # print(ll_hemi_image_urls)
+    browser.quit()
+    return (ll_hemi_image_urls)
+
+
+
+
+def init_browser():
+    # @NOTE: Replace the path with your actual path to the chromedriver
+    # executable_path = {"executable_path": "/usr/local/bin/chromedriver"}
+    executable_path = {'executable_path': 'chromedriver.exe'}
+    return Browser("chrome", **executable_path, headless=False)
+
+def scrape():
+    # # Scrape everything
+
+    # this dictionary will hold everything we pull from all the sites
+    scraped_data = {}
+
+    news_title, news_p = scrape_news()
+    print(news_title, news_p)
+    scraped_data['news_title'] = news_title
+    scraped_data['news_p'] = news_p
+
+    featured_image_url, featured_image_caption = scrape_jpl()
+    scraped_data['featured_image_url'] = featured_image_url
+    scraped_data['featured_image_caption'] = featured_image_caption
+
+    wx_text = scrape_twitter()
+    scraped_data['weather_data'] = wx_text
+
+    facts_html_str = scrape_facts()
+    scraped_data['facts_html_str'] = facts_html_str
+
+    ll_hemi_image_urls = scrape_img_urls()
     scraped_data['hemisphere_image_urls'] = ll_hemi_image_urls
 
-    # # Example:
-    # hemisphere_image_urls = [
-    #     {"title": "Valles Marineris Hemisphere", "img_url": "..."},
-    #     {"title": "Cerberus Hemisphere", "img_url": "..."},
-    #     {"title": "Schiaparelli Hemisphere", "img_url": "..."},
-    #     {"title": "Syrtis Major Hemisphere", "img_url": "..."},
-    # ]
-
-
-    # In[12]:
-
-
-    # Close the browser
-    browser.quit()
-
-    # Print the data gather out
-    # print(scraped_data)
-
-
-    # In[13]:
-
-
-    # File-> download as python into a new module called scrape_mars.py
-
-
-    # In[14]:
-
-
-    # use day 3 09-Ins_Scrape_And_Render/app.py as a blue print on how to finish the homework.
-
-    # replace the contents of def index() and def scraper() appropriately.
-
-    # change the index.html to render the site with all the data.
-
+ 
     return(scraped_data)
